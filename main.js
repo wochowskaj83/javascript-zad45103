ba26cfc3479935389709946bce41dc2284d8eb63
wochowskaj83 <wochowskaj@gmail.com>
2023-04-13 17:20:29 +0200

    rozwiazanie zadania

---
+++ main.js
@@ -0,0 +1,16 @@
+let myNumber = 210;
+
+function przedzial(n) {
+    if (n<100) {
+        return "Liczba nie znajduje się w przedziale";
+    }
+    else if (n>200) {
+        return "Liczba nie znajduje się w przedziale";
+    }
+    else {
+    return "liczba znajduje się w przedziale";
+}}
+
+console.log(przedzial(myNumber))
+
+
